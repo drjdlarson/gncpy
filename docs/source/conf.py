595a373f5b064bbdb6529e3af88fa9f3e72a9822
ryan4984 <ryan4984@gmail.com>
2021-09-13 06:45:53 -0500

    removing html source links from docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,6 +80,7 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
 }
+html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
