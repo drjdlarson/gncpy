a29a7ce732d2deb12a36451b88d450c0e3abf503
ryan4984 <ryan4984@gmail.com>
2023-03-15 01:52:21 +0000

    added vs code dev container for easier toolchain setup when developing for the package

---
+++ scripts/install-dev-tools.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git config --global diff.submodule log
+git config --global status.submodulesummary 1
+git config --global submodule.recurse true
+git submodule update --init --recursive
+
+pip install -e serums
