10ee2b56654121c79cd2752a0829599e04eae7fe
ryan4984 <ryan4984@gmail.com>
2023-04-09 15:41:45 +0000

    remove conan stuff, cleanup cmake, cleanup pyproject.toml

--- recipe/test_package/src/example.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <gncpy/dynamics/DoubleIntegrator.h>
-
-int main() {
-    lager::gncpy::dynamics::DoubleIntegrator<double> obj(0.5);
-    std::cout << "Address of double integrator " << &obj << std::endl;
-
-    return 0;
-}
