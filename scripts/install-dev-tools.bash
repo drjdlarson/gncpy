3a424344b1669628f643ba8981a9bfd23d7923bf
ryan4984 <ryan4984@gmail.com>
2023-05-11 16:27:27 -0600

    cleanup devcontainer and dockerfile

--- scripts/install-dev-tools.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config --global diff.submodule log
-git config --global status.submodulesummary 1
-git config --global submodule.recurse true
-# git submodule update --init --recursive
-
-pip install -e serums
