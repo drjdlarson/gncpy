9bd64005b62b60b8ef616b1f2a53c3e138b450b3
ryan4984 <51098111+ryan4984@users.noreply.github.com>
2023-05-29 11:26:15 -0600

    hotfix for importing BayesFilter into filters package (#19)

--- src/gncpy/filters/__init__.py
+++ src/gncpy/filters/__init__.py
@@ -1,6 +1,9 @@
 """Definitions for common filters."""
+from gncpy.filters.bayes_filter import BayesFilter
 from gncpy.filters.bootstrap_filter import BootstrapFilter
-from gncpy.filters.ekf_gaussian_scale_mixture_filter import EKFGaussianScaleMixtureFilter
+from gncpy.filters.ekf_gaussian_scale_mixture_filter import (
+    EKFGaussianScaleMixtureFilter,
+)
 from gncpy.filters.extended_kalman_filter import ExtendedKalmanFilter
 from gncpy.filters.gci_filter import GCIFilter
 from gncpy.filters.imm_gci_filter import IMMGCIFilter
@@ -10,11 +13,17 @@ from gncpy.filters.kf_gaussian_scale_mixture_filter import KFGaussianScaleMixtur
 from gncpy.filters.max_corr_ent_ukf import MaxCorrEntUKF
 from gncpy.filters.max_corr_ent_upf import MaxCorrEntUPF
 from gncpy.filters.particle_filter import ParticleFilter
-from gncpy.filters.qkf_gaussian_scale_mixture_filter import QKFGaussianScaleMixtureFilter
+from gncpy.filters.qkf_gaussian_scale_mixture_filter import (
+    QKFGaussianScaleMixtureFilter,
+)
 from gncpy.filters.quadrature_kalman_filter import QuadratureKalmanFilter
-from gncpy.filters.sqkf_gaussian_scale_mixture_filter import SQKFGaussianScaleMixtureFilter
+from gncpy.filters.sqkf_gaussian_scale_mixture_filter import (
+    SQKFGaussianScaleMixtureFilter,
+)
 from gncpy.filters.square_root_qkf import SquareRootQKF
 from gncpy.filters.students_t_filter import StudentsTFilter
-from gncpy.filters.ukf_gaussian_scale_mixture_filter import UKFGaussianScaleMixtureFilter
+from gncpy.filters.ukf_gaussian_scale_mixture_filter import (
+    UKFGaussianScaleMixtureFilter,
+)
 from gncpy.filters.unscented_kalman_filter import UnscentedKalmanFilter
 from gncpy.filters.unscented_particle_filter import UnscentedParticleFilter
