207a2ac045dad8d32d06c33fc28ce2ad2b7d5f91
vaweirens <vaweirens@crimson.ua.edu>
2023-07-03 15:46:57 -0400

    initial cwh interface commit

--- src/gncpy/dynamics/basic.py
+++ src/gncpy/dynamics/basic.py
@@ -1160,6 +1160,54 @@ class ClohessyWiltshireOrbit(ClohessyWiltshireOrbit2d):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.__controlParams = cpp_bindings.ControlParams()
+        self.__stateTransParams = cpp_bindings.StateTransParams()
+        self.__model = cpp_bindings.DoubleIntegrator(0.1)
+
+    @property
+    def allow_cpp(self):
+        return True
+
+    # must be provided if allow_cpp is true
+    def args_to_params(self, state_args, control_args):
+        if len(state_args) != 1:
+            raise RuntimeError(
+                "state args must be only (dt,) not {}".format(repr(state_args))
+            )
+
+        if len(control_args) != 0 and self.control_model is None:
+            warn("Control agruments supplied but no control model specified")
+        elif self.control_model is not None:
+            try:
+                self.__controlParams = self.control_model.args_to_params(control_args)
+            except Exception:
+                warn(
+                    "Supplied control model does not support c++ backend but model is supposed to allow c++ backend. Not generating parameters"
+                )
+                self.__controlParams = cpp_bindings.ControlParams()
+
+        # hack since state params is empty but things are set in the model
+        self.__model.dt = state_args[0]
+        return self.__stateTransParams, self.__controlParams
+
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def state_names(self):
+        return self.__model.state_names()
+
+    def propagate_state(self, timestep, state, u=None, state_args=None, ctrl_args=None):
+        if state_args is None:
+            raise RuntimeError("state_args must be (dt,) not None")
+        if self.control_model is None:
+            self.__model.dt = state_args[0]
+            return self.__model.propagate_state(timestep, state).reshape((-1, 1))
+        else:
+            return super().propagate_state(
+                timestep, state, u=u, state_args=state_args, ctrl_args=ctrl_args
+            )
 
     def get_dis_process_noise_mat(self, dt):
         """Calculates the process noise.
