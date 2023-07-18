88e3ef27791f419300620a5cbf3fd2e4777cda36
ryan4984 <ryan4984@gmail.com>
2023-07-17 19:01:20 -0600

    start to update basic dynamics structure

---
+++ src/gncpy/dynamics/basic/__init__.py
@@ -0,0 +1,7 @@
+"""Basic models and classes that can be extended.
+
+These provide an easy to use interface for some common dynamic objects and
+and their associated models. They have been designed to integrate well with the
+filters in :mod:`gncpy.filters`.
+"""
+from .clohessy_wiltshire_orbit2d import ClohessyWiltshireOrbit2d
